fca71d06ab7bcd7a8a687d67c87d9518df77f88a
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2022-11-20 22:52:31 +0900

    Add 주석

--- Python Baekjoon/b2309.py
+++ Python Baekjoon/b2309.py
@@ -5,7 +5,7 @@ for _ in range(9):
     heights.append(int(input()))
 
 while(sum(heights) != 100):
-    fake = random.sample(heights, 2)
+    fake = random.sample(heights, 2) #heights 리스트에서 랜덤으로 2개의 숫자 뽑기
     if (sum(fake) == sum(heights) - 100):
         for j in fake:
             heights.remove(j)
