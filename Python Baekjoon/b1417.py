7590afb7aeb27f9014b2666d290ddb0d7b7bc4b4
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2022-11-24 15:30:21 +0900

    22.11.23

---
+++ Python Baekjoon/b1417.py
@@ -0,0 +1,20 @@
+n = int(input())
+candidate = []
+count = 0
+
+for _ in range(n):
+    candidate.append(int(input()))
+
+while (1):
+    if (candidate[0] == max(candidate) and candidate.count(max(candidate)) == 1):
+        break
+
+    if (n != 1):
+        i = candidate.index(max(candidate), 1, n)
+
+        candidate[i] -= 1
+        candidate[0] += 1
+        count += 1
+
+
+print(count)
