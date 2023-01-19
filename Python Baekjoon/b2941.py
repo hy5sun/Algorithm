2f48232a8f355911bff8b8c1f53679636967882a
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-19 18:56:24 +0900

    style: b2941

--- Python Baekjoon/b2941.py
+++ Python Baekjoon/b2941.py
@@ -1,8 +1,7 @@
 word = input()
 croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-for i in range(len(croatia)):
-    if croatia[i] in word:
-        word = word.replace(croatia[i], "1")
+for char in croatia:
+    word = word.replace(char, "1")
 
 print(len(word))
