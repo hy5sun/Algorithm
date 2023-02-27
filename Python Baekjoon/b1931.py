a31334ad79efb89c1a844067ba7d167c3e190d9c
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-02-27 23:48:10 +0900

    23.02.27

--- Python Baekjoon/b1931.py
+++ Python Baekjoon/b1931.py
@@ -1,4 +1,4 @@
-# 그리디 알고리즘 (해결X)
+# 그리디 알고리즘 (시간초과..)
 n = int(input())
 time = []
 meetingTime = []
@@ -6,19 +6,19 @@ count = 0
 
 for i in range(n):
     start, end = map(int, input().split())
-    time.append([start, end, end - start])
+    spending = end - start
+    time.append([start, end, spending])
 
-time.sort()
-time.sort(key=lambda time: time[2])
+    time.sort()
+    time.sort(key=lambda time: time[2])
 
-for i in range(n):
-    a = [x for x in range(time[i][0], time[i][1])]
-    for j in a:
-        if j in meetingTime:
-            break
-    meetingTime.append(q for q in a)
-    count += 1
+    for x in range(start, end):
+        if x in meetingTime:
+            spending -= 1
+
+    if spending == end - start:
+        count += 1
+        for x in range(start, end):
+            meetingTime.append(x)
 
 print(count)
-
-# [3, 5], [5, 7], [12, 14], [1, 4], [8, 11]
