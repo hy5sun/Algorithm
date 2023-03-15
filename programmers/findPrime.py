b411787907142091d7f228ffce37fc68b39c46ef
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-03-15 19:29:05 +0900

    refactoring(2)

--- programmers/findPrime.py
+++ programmers/findPrime.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools, math
 numbers = input()
 answer = 0
 num = set()
@@ -10,7 +10,7 @@ num -= set(range(2))
 
 for n in num:
     prime = True
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n)+1)):
         if n % i == 0:
             prime = False
             break
