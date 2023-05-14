76c5da7f71741d3e7faf4ddd2561c688d64e10dd
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-05-14 22:43:03 +0900

    refactor

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -2,8 +2,6 @@ def solution(elements):
     answer = set()
     double = elements + elements
     for i in range(len(elements)):
-        j = 0
-        for _ in range(len(elements)):
+        for j in range(len(elements)):
             answer.add(sum(double[i:i+j+1]))
-            j += 1
     return len(answer)
