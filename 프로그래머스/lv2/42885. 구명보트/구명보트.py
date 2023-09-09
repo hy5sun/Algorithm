bc5dc4f6767cc2edf967d26f484e1727269d4508
Hyosun <84787653+hy5sun@users.noreply.github.com>
2023-09-09 13:52:04 +0900

    [level 2] Title: 구명보트, Time: 7.39 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+    left, right = 0, len(people)-1
+    while left <= right:
+        if people[left] <= limit - 40 and people[left] + people[right] <= limit:
+            right -= 1
+        answer += 1
+        left += 1
+    return answer
