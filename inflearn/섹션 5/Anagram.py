66da787300429f55b66825dfc39e7fe0bbe6a9b3
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-03-03 15:02:22 +0900

    Anagram (리팩토링)

--- inflearn/섹션 5/Anagram.py
+++ inflearn/섹션 5/Anagram.py
@@ -2,13 +2,10 @@ def Anagram(wordA, wordB):
     words = {}
 
     for wa in wordA:
-        if wa not in words:
-            words[wa] = 1
-        else:
-            words[wa] += 1
+        words[wa] = words.get(wa, 0) + 1
 
     for wb in wordB:
-        words[wb] -= 1
+        words[wb] = words.get(wb, 0) - 1
 
     for w, cnt in words.items():
         if cnt != 0:
