dc9d77b67c21556c004fd7de4cb360f36dfe3d35
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-06-29 16:21:34 +0900

    최대 점수 구하기 (DFS) 리팩토링

--- inflearn/섹션 7/최대 점수 구하기.py
+++ inflearn/섹션 7/최대 점수 구하기.py
@@ -2,15 +2,12 @@ def DFS(v, score, time):
     global max_score
     if time < 0:
         return
-    last_score = sum(a[0] for a in lst[v:])
-    if score + last_score < max_score:
-        return
-    if v == n-1 and time >= 0:
+    if v == n and time >= 0:
         if score > max_score:
             max_score = score
         return
     else:
-        DFS(v+1, score+lst[v+1][0], time-lst[v+1][1])
+        DFS(v+1, score+lst[v][0], time-lst[v][1])
         DFS(v+1, score, time)
 
 if __name__ == "__main__":
@@ -19,6 +16,5 @@ if __name__ == "__main__":
     max_score = 0
     for _ in range(n):
         lst.append(list(map(int, input().split())))
-    lst.sort(reverse=True)
-    DFS(-1, 0, m)
+    DFS(0, 0, m)
     print(max_score)
