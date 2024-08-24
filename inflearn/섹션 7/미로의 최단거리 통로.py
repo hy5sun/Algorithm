b100052b6148b6d47fe4fe9852d5d7e23b528392
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-08-24 17:37:25 +0900

    미로의 최단거리 통로 (BFS) 리팩토링

--- inflearn/섹션 7/미로의 최단거리 통로.py
+++ inflearn/섹션 7/미로의 최단거리 통로.py
@@ -5,20 +5,22 @@ maze_map = []
 for _ in range(7):
     maze = list(map(int, input().split()))
     maze_map.append(maze)
-queue = deque([[0, 0, 0]])
-ch = [[0] * 7 for _ in range(7)]
+queue = deque([[0, 0]])
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+cnt = [[0] * 7 for _ in range(7)]
 
 while queue:
     player = queue.popleft()
-    if player[0] == 6 and player[1] == 6:
-        print(player[2])
+    if player == [6, 6]:
+        print(cnt[6][6])
         sys.exit()
-    if 0 <= player[0] < 7 and 0 <= player[1] < 7:
-        if maze_map[player[0]][player[1]] == 0 and ch[player[0]][player[1]] == 0:
-                ch[player[0]][player[1]] = 1
-                queue.append([player[0]+1, player[1], player[2]+1])
-                queue.append([player[0], player[1]+1, player[2]+1])
-                queue.append([player[0]-1, player[1], player[2]+1])
-                queue.append([player[0], player[1]-1, player[2]+1])
+    for i in range(4):
+        x = player[0] + dx[i]
+        y = player[1] + dy[i]
+        if 0 <= x < 7 and 0 <= y < 7 and maze_map[x][y] == 0:
+            maze_map[x][y] = 1
+            cnt[x][y] = cnt[player[0]][player[1]] + 1
+            queue.append([x, y])
 
 print(-1)
